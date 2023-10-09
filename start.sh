986563fc6e5cd445b772206e9f1c669dfcfc34d7
NxtStark <85378792+Nxt-Stark@users.noreply.github.com>
2023-10-10 00:54:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
-if [ -z $UPSTREAM_REPO ]
+if [ -z "$UPSTREAM_REPO" ]
 then
   echo "Cloning main Repository"
   git clone https://github.com/RJMALLU/THEROCK2.git /THEROCK2 
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /THEROCK2
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /THEROCK2
 fi
 cd /THEROCK2 
 pip3 install -U -r requirements.txt
